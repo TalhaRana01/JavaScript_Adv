f3ad2932845cb9087b5bb2f3bcfa5221ba112119
Talha Rana <mtalharana093@gmail.com>
2024-03-15 18:03:01 +0500

    truthy%falsyValue

---
+++ JAVASCRIPT/basic_04/truthyValue.js
@@ -0,0 +1,30 @@
+// for Checking Truthy and Falsy Values for use if else statement 
+
+
+//const val1 = 0      // zero is a falsy value:
+// const val1 = "";   // Empty String is a Falsy value:
+// const val1 = null;  // Null is falsy value:
+// const val1 = []     // Empty Array is a Truthy Value:
+
+
+if (val1) {
+    console.log("truthy value:");
+
+}else{
+    console.log("Falsy Value");
+}
+
+
+
+
+
+
+
+// const userEmail = "mtalhaRana093@gmail.com"
+
+// if (userEmail) {
+//     console.log("Got the Email: ");
+    
+// }else{
+//     console.log("Did not get the email:");
+// }
