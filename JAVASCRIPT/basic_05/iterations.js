cbae30d79b14ed631fe3417f719a39027171c493
Talha Rana <mtalharana093@gmail.com>
2024-03-15 23:58:34 +0500

    For Loop

---
+++ JAVASCRIPT/basic_05/iterations.js
@@ -0,0 +1,51 @@
+// For Loop
+
+// let array = ["Talha", "Ali", "Danyal", "Nabeel", "Talha", "Ali", "Danyal", "Nabeel", "Khan" , ]
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     console.log(element)
+
+// }
+
+// for (let i = 1; i < 10 ; i++) {
+//     console.log(`Outer Loop : ${i}`)
+//     for (let j  = 1; j  < 10; j ++) {
+//         // console.log(`Inner Loop Value : ${j} and Inner Loop ${i}` )
+//         console.log( i  + '*' + j + '=' + j*i);
+
+// }
+
+// }
+
+//  let myArray = ["Talha", "Ali", "Danyal", "Nabeel", "Talha", "Ali", "Danyal", "Nabeel", "Khan" , ]
+// // console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     // console.log(element)
+    
+// }
+
+//Break and Continue
+
+
+// BREAK KEYWORD
+// for (let i = 1; i <= 25; i++) {
+//     if(i ==7){
+//         console.log("5 Deducted")
+//         break;
+//     }
+//    console.log(`Value of index is : ${i}`)
+    
+// }
+
+
+// CONTINUE KEYWORD
+for(let i = 1; i <= 25; i++) {
+    if(i ==7){
+        console.log("5 Deducted")
+        continue;
+    }
+   console.log(`Value of index is : ${i}`)
+    
+}
